1bc1bef4e9cf13462069b2128385dff496b0c16b
Trainee 29 <umesh.more@raweng.com>
2021-02-08 22:05:49 +0530

    Added Router & Pages

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,22 +1,25 @@
-import { Route, Router, Switch } from "react-router-dom";
+import { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "../pages/home/Home";
 import NotFound from "../pages/notFound/NotFound";
 import SignIn from "../pages/signin/SignIn";
 import SignUp from "../pages/signup/SignUp";
 
-const RouterRoute = () => {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/signUp" exact component={SignUp} />
-          <Route path="/signIn" exact component={SignIn} />
-          <Route exact component={NotFound} />
-        </Switch>
-      </Router>
-    </>
-  );
-};
+class RouterRoute extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signUp" exact component={SignUp} />
+            <Route path="/signIn" exact component={SignIn} />
+            <Route exact component={NotFound} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
+}
 
 export default RouterRoute;
