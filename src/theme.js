8d260df934bdbd4de024f951f9c944e84ac100df
Trainee 29 <umesh.more@raweng.com>
2021-02-22 18:12:35 +0530

    added media

--- src/theme.js
+++ src/theme.js
@@ -12,6 +12,15 @@ const theme = createMuiTheme({
       main: "#fff",
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
 
 export { theme };
